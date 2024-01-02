cb986f1bf5eeca9977206947d75d54404e3d1a85
ZetkoOfficial <info@zetkoofficial.com>
2024-01-02 19:23:00 +0100

    implemenirano več trakov in dodani testi za 1-trak deterministične

---
+++ lib/test.ml
@@ -0,0 +1,66 @@
+open Turing
+
+let tiri = List.mapi (fun glava l -> { glava; trak = l |> List.to_seq |> Trak.of_seq })[
+  [(0,Podatek 0);(1,Podatek 1);(2,Podatek 2);];
+  [(0,Podatek 1);(1,Podatek 2);(2,Podatek 3);];
+  [(0,Podatek 2);(1,Podatek 3);(2,Podatek 4);]
+];;
+
+(** unsafe  *)
+let pod_to_int podatek = 
+  match podatek with
+  | Podatek p -> p
+  | Prazno -> 0 
+;;
+(** unsafe *)
+let tir_to_list tir = tir.trak |> Trak.bindings |> List.map (fun (i,v) -> (i,pod_to_int v));;
+
+(* Testi za branje in pisanje na tire. *)
+
+let%test_unit "preberi_tire" = [%test_eq: Base.int Base.list] 
+  ( tiri |> preberi_tire |> List.map (pod_to_int) ) 
+  ([0;2;4])
+;;
+
+let%test_unit "posodobi_tire" = [%test_eq: Base.int Base.list] 
+  ( 
+    (posodobi_tire tiri [Podatek 3,Desno; Podatek 3,Levo; Podatek 10, N]) 
+    |> preberi_tire |> List.map (pod_to_int) 
+  ) 
+  ([1;1;10])
+;;
+
+let%test_unit "nastavi_input" = [%test_eq: ((Base.int*Base.int) Base.list) Base.list] 
+  (
+    let tiri' = nastavi_input 0 [Podatek 6; Podatek 6; Podatek 6; Podatek 6; ] tiri in
+    List.map (fun t -> tir_to_list t) tiri'
+  )
+  ([[0,6;1,6;2,6;3,6];[0,1;1,2;2,3];[0,2;1,3;2,4]])
+;;
+
+(* 
+    Test determinstičnega stroja z enim tirom 
+    (busy beaver 3 stanja 2 simbola iz https://en.wikipedia.org/wiki/Busy_beaver)     
+*)
+
+let tranzicije = [
+  (Stanje "A", [Prazno]),     (Stanje "B", [Podatek 1,Desno]);
+  (Stanje "A", [Podatek 1]),  (Stanje "H", [Podatek 1,Desno]);
+
+  (Stanje "B", [Prazno]),     (Stanje "C", [Prazno,Desno]);
+  (Stanje "B", [Podatek 1]),  (Stanje "B", [Podatek 1,Desno]);
+
+  (Stanje "C", [Prazno]),     (Stanje "C", [Podatek 1,Levo]);
+  (Stanje "C", [Podatek 1]),  (Stanje "A", [Podatek 1,Levo]);
+]
+
+let turing = ustvari_turing 1 (Stanje "A") [Stanje "H"] tranzicije;;
+
+let%test_unit "pozeni busy_beaver" = [%test_eq: (Base.int*Base.int) Base.list ] 
+  (
+    match pozeni turing [] with
+    | Konec turing -> tir_to_list @@ List.hd turing.data.tiri
+    | _ -> failwith "neuspešno"
+  )
+  ([ -1,1; 0,1; 1,1; 2,1; 3,1; 4,1 ])
+;;
--- lib/turing.ml
+++ lib/turing.ml
@@ -13,27 +13,35 @@ type 'p tir = {
   glava:    int
 }
 
-(* Tipa predstavlja trenutno stanje neke turingove naprave z enim tirom in obliko njegove tranzicicje *)
-type ('s, 'p) turing1_data = {
-  tir:      'p tir;
+(* Tipa predstavlja trenutno stanje neke turingove naprave z večimi tiri in obliko njegove tranzicicje *)
+type ('s, 'p) turing_data = {
+  tiri:     'p tir list;
   stanje:   's stanje
 }
-type ('s, 'p) turing1_tranzicija = ('s stanje * 'p podatek) * ('s stanje * 'p podatek * smer)
+type ('s, 'p) turing_tranzicija = ( 's stanje * ('p podatek list) ) * ( 's stanje * ('p podatek * smer) list )
 
 (* 
-    Turingovo napravo z enim tirom predstavimo z trenutnim stanjem naprave,
+    Turingovo napravo z večimi tirom predstavimo z trenutnim stanjem naprave,
     seznamom tranzicij in seznamom sprejemnih stanj. 
 *)
-type ('s, 'p) turing1 = {
-  data:               ('s, 'p) turing1_data;
+type ('s, 'p) turing = {
+  data:               ('s, 'p) turing_data;
   sprejemna_stanja:   ('s stanje) list;
-  tranzicije:         ('s, 'p) turing1_tranzicija list
+  tranzicije:         ('s, 'p) turing_tranzicija list
 }
 
-(** Iz začetnega stanja, seznama sprejemnih stanj in tranzicij ustvari turingovo napravo z enim tirom.  *)
-let ustvari_turing1 zacetno_stanje sprejemna_stanja tranzicije: ('s, 'p) turing1 = 
+(** Pomožna funkcija ki ustvari seznam [n] tirov. *)
+let ustvari_tire n = 
+  let rec aux n acc = 
+    if n = 0 then acc
+    else aux (n-1) ({trak=Trak.empty; glava=0}::acc) in
+  aux n []
+;;
+
+(** Iz začetnega stanja, seznama sprejemnih stanj in tranzicij ustvari turingovo napravo z [n] tiri.  *)
+let ustvari_turing n zacetno_stanje sprejemna_stanja tranzicije: ('s, 'p) turing = 
   let data = {
-    tir = { trak = Trak.empty; glava = 0 };
+    tiri = ustvari_tire n;
     stanje = zacetno_stanje
   } in { data; sprejemna_stanja; tranzicije }
 ;;
@@ -41,11 +49,24 @@ let ustvari_turing1 zacetno_stanje sprejemna_stanja tranzicije: ('s, 'p) turing1
 type ('a,'b) rezultat = Konec of 'a | Naprej of 'a | Napaka of 'a
 
 (* Pomagalni funckiji za branje tirov in iskanje ujemajočih tranzicij. *)
-let preberi_tir tir = 
-  match Trak.find_opt tir.glava tir.trak with
-  | Some p -> p | None -> Prazno
+let preberi_tire tiri = 
+  List.map (fun tir -> 
+    match Trak.find_opt tir.glava tir.trak with
+    | Some p -> p | None -> Prazno
+  ) tiri
 ;;
 let ujemajoca_tranzicija t_in (t_in',_) = (t_in = t_in');;  
+
+let posodobi_tire tiri pisanje = 
+  List.map2 (fun tir (p,smer) ->
+    let glava = match smer with
+      | Levo  -> tir.glava-1
+      | Desno -> tir.glava+1
+      | N     -> tir.glava in
+
+    { trak = Trak.add tir.glava p tir.trak; glava }
+  ) tiri pisanje
+;;
 
 (** Funkcija vrne turingove stroje po enem koraku nedeterminističnega turingovega stroja. *)
 let korak turing = 
@@ -53,31 +74,25 @@ let korak turing =
 
   if List.mem data.stanje turing.sprejemna_stanja then [ Konec turing ]
   else
-    let s,p = data.stanje, preberi_tir data.tir  in
+    let s,p = data.stanje, preberi_tire data.tiri in
     let tranzicije = List.find_all (ujemajoca_tranzicija (s,p)) turing.tranzicije in
     if tranzicije = [] then [ Napaka turing ]
     else 
-      List.map (fun (_, (s',p',smer)) -> 
-        let glava = match smer with
-        | Levo  -> data.tir.glava-1
-        | Desno -> data.tir.glava+1
-        | N     -> data.tir.glava in
-
+      List.map (fun (_, (s', pisanje)) -> 
         let data = {
-          tir = {
-            trak = Trak.add data.tir.glava p' data.tir.trak; 
-            glava 
-          };
+          tiri = posodobi_tire data.tiri pisanje;
           stanje = s' 
         } in Naprej { turing with data }
       ) tranzicije
 ;;
 
-(** Od [i] naprej nastavlja vrednosti traku na vrednosti v seznamu [input].  *)
-let rec nastavi_trak i input trak = 
+(** Od [i] naprej nastavlja vrednosti [prvega] traku na vrednosti v seznamu [input].  *)
+let rec nastavi_input i input tiri = 
   match input with
-  | [] -> trak
-  | h::t -> nastavi_trak (i+1) t (Trak.add i h trak)
+  | [] -> tiri
+  | h::t -> 
+    let h',t' = List.hd tiri, List.tl tiri in
+    nastavi_input (i+1) t ( {h' with trak = Trak.add i h h'.trak} :: t' )
 ;;
 
 (** Požene deteministični turingov stroj z podanim [input]-om. *)
@@ -90,6 +105,6 @@ let pozeni turing input =
     | _ -> print_endline "Stroj ni determinističen!"; Napaka turing in
 
   let data = { turing.data with
-    tir = { turing.data.tir with trak = nastavi_trak 0 input turing.data.tir.trak };
+    tiri = nastavi_input 0 input turing.data.tiri
   } in aux {turing with data}
 ;;
